f6297a906a797777456d77ccf71dc6b3128debb5
João Gomes <60653037+JoaoGomes5@users.noreply.github.com>
2020-04-14 22:13:51 +0100

    TESTEW1

--- assets/content/icon_admin.php
+++
@@ -1,2 +0,0 @@
-
-<a href="help/home.php" class="float"><i class="fas fa-user-headset fa-4x"></i></a>
--- assets/content/icon_home.php
+++
@@ -1,3 +0,0 @@
-<a href="admin/dist/login.html" class="float-home"><i class="fas fa-user-cog fa-2x my-float"></i></a>
-
-
